9b7b92cd1443cb1c195f61f9cf433dd8a99d4452
Nic Boie <nic@webvolta.com>
2014-08-08 23:50:12 -0500

    Coveralls cleanup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,15 @@
 require 'bundler/setup'
 Bundler.setup
 
+require 'coveralls'
+Coveralls.wear!
+
 require 'simplecov'
 SimpleCov.start
 
 Dir[File.dirname(__FILE__) + "/../lib/**/*.rb"].each do |file|
   require file
 end
-
-require 'coveralls'
-Coveralls.wear!
 
 begin
   require 'pry'
